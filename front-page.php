8de64e52280276276d822cf39fa11e43c8662269
JesusShocklogic <jesus@shocklogic.com>
2023-01-25 08:57:40 -0400

    Removing unused areas from the dashboard

--- front-page.php
+++ front-page.php
@@ -7,9 +7,7 @@ get_header();
 include_slick_style();
 
 $settings = get_id_by_slug('settings');
-?>
 
-<?php
 if (have_posts()) {
     while (have_posts()) {
         the_post(); ?>
@@ -51,85 +49,6 @@ if (have_posts()) {
             </div>
         <?php } ?>
 
-        <!-- Section 4 -->
-        <?php if (get_field('section_4_enable')) { ?>
-            <div class="st-p bg-2">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="title"><?php the_field('s4_title'); ?></div>
-                            <div><?php the_field('s4_content'); ?></div>
-                        </div>
-                        <div class="col-12">
-                            <?= get_template_part('tpl/partners', 'index') ?>
-                        </div>
-                        <div class="col-12 text-center pt-5">
-                            <?php print_button('s4_button', get_the_ID()); ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        <?php } ?>
-
-        <!-- Section 5 - Blog-->
-        <?php
-        if (get_field('display_blog')) {
-        ?>
-            <div class="st-p bg-1">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="title"><?php the_field('s5_title'); ?></div>
-                        </div>
-                        <div class="col-12">
-                            <?= get_template_part('tpl/blog', 'index') ?>
-                        </div>
-                        <div class="col-12 text-right pt-5">
-                            <?php $link = get_field('s5_link'); ?>
-                            <a class="readMore" href="<?= $link['url'] ?>" target="<?= $link['target'] ?>"><?= $link['title'] ?></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        <?php } ?>
-
-
-        <!-- Section 6 -->
-        <?php the_content() ?>
-        <?php
-        if (get_field('section_6_enable')) {
-
-            $sponsors = get_field('sponsors_group');
-            $published_posts = wp_count_posts("sponsors");
-            if ($sponsors['title'] || $published_posts->publish > 0) {
-        ?>
-                <div class="st-p d-none">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="title"><?php echo $sponsors['title'] ?></div>
-                                <div><?php echo $sponsors['text'] ?></div>
-                            </div>
-                            <div class="col-12">
-                                <?= get_template_part('tpl/sponsors', 'index') ?>
-                            </div>
-                            <div class="col-12 text-center pt-5">
-                                <?php
-                                $link = $sponsors['link'];
-                                if ($link) :
-                                    $link_url = $link['url'];
-                                    $link_title = $link['title'];
-                                    $link_target = $link['target'] ? $link['target'] : '_self';
-                                ?>
-                                    <a class="btn px-4 py-2" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
-                                <?php endif;
-                                ?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-        <?php }
-        } ?>
 
         <!-- Section 7 -->
         <?php if (get_field('section_7_enable')) { ?>
@@ -138,9 +57,6 @@ if (have_posts()) {
                     <div class="row">
                         <div class="col-12">
                             <div class="title"><?php the_field('s6_title'); ?></div>
-                        </div>
-                        <div class="col-12">
-                            <?= get_template_part('tpl/testimonials', 'index') ?>
                         </div>
                     </div>
                 </div>
@@ -155,8 +71,7 @@ if (have_posts()) {
 else {
     //No content Found
 } // end else
-?>
-<?php
+
 get_footer();
 include_slick_script();
 include_slick_settings();
